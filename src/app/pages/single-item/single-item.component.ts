6fe72b1c4082522c56632dc34088620448fc97c9
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2024-01-12 20:16:46 +0400

    single page data done

--- src/app/pages/single-item/single-item.component.ts
+++ src/app/pages/single-item/single-item.component.ts
@@ -1,27 +1,42 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/shared/data.service';
 
 @Component({
   selector: 'app-single-item',
   templateUrl: './single-item.component.html',
   styleUrls: ['./single-item.component.scss']
 })
-export class SingleItemComponent {
-
+export class SingleItemComponent implements OnInit {
+  singleItemData: any[] = [];
+  
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient,
-    private elementRef: ElementRef,
+    private data: DataService
   ) {}
 
   ngOnInit(): void {
-    this.getItem()
+    this.getItem();
   }
 
   getItem() {
     const itemId = this.route.snapshot.paramMap.get('id');
-    
+    if (itemId) {
+      this.data.getMensData().subscribe((res) => {
+        const menObject = res?.men;
+        if (menObject) {
+          const selectedShoe = Object.values(menObject).find(shoe => shoe.id.toString() === itemId);
+          if (selectedShoe) {
+            this.singleItemData = [...this.singleItemData, selectedShoe];
+            console.log(this.singleItemData)
+          }
+        }
+      });
+    }
   }
-
+  
+  
 }
+
